5422b1a47f65299b1ca432389f22d8ecf5b04678
rarishine <rarishine@gmail.com>
2014-02-17 18:04:29 +0530

    i field named friends is added

--- com.calp.sampl/grails-app/domain/com/calp/sampl/MeTesting.groovy
+++ com.calp.sampl/grails-app/domain/com/calp/sampl/MeTesting.groovy
@@ -5,12 +5,13 @@ String name
 String job
 String test1
 
-
+String friends
 
     static constraints = {
 		name(blank:false)
 		job(blank:false)
 		test1(blank:false)
+		friends(blank:false)
 	
     }
 }
